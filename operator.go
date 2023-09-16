43f8ba2c341347c10fc84dc486c4064e0e2c886d
kee_hoon <djfkfk12345@naver.com>
2023-09-16 17:43:51 +0900

    230916

--- operator.go
+++ operator.go
@@ -58,6 +58,7 @@ func main() {
 	num >>= 8 // 1000000000 의 비트를 오른쪽으로 8칸 이동
 	fmt.Printf("num <<= 9 -> %08b, %d\n", num, num)
 
+	// 논리 연산자
 	var b bool = true
 	c := false
 
@@ -74,5 +75,51 @@ func main() {
 	// NOT 연산
 	fmt.Println("!1 ", !true)
 	fmt.Println("!0 ", !false)
+
+	// 관계 연산자
+	fmt.Println("\n13 == 13 :", 13 == 13)
+	fmt.Println("13 == 23 :", 13 == 23)
+	fmt.Println("13 != 13 :", 13 != 13)
+	fmt.Println("3 != 5 :", 3 != 5)
+	fmt.Println("0 < 1 :", 0 < 1)
+	fmt.Println("0 > 1 :", 0 > 1)
+	fmt.Println("0 >= 1 :", 0 >= 1)
+	fmt.Println("0 <= 1 :", 0 <= 1)
+
+	// 비트 연산자
+	num11 := 15        // 00001111
+	var num12 int = 20 // 00010100
+
+	fmt.Printf("\nnum11 & num12 : %08b, %d\n", num11&num12, num11&num12)  // AND
+	fmt.Printf("num11 | num12 : %08b, %d\n", num11|num12, num11|num12)    // OR
+	fmt.Printf("num11 ^ num12 : %08b, %d\n", num11^num12, num11^num12)    // XOR
+	fmt.Printf("num11 &^ num12 : %08b, %d\n", num11&^num12, num11&^num12) // NAND
+
+	fmt.Printf("num11 << 4 : %08b, %d\n", num11<<4, num11<<4) // 00001111비트를 왼쪽으로 4칸 이동
+	fmt.Printf("num12 >> 4 : %08b, %d\n", num12>>2, num12>>2) // 00001111비트를 오른쪽으로 2칸 이동
+
+	// 채널 연산자 : Go에만 있는 개념
+	// 채널은 고루틴(goroutine)끼리 데이터를 주고받고 실행의 흐름을 제어하는 기능
+	// <-는 채널의 수신을 연산합니다. 채널에 값을 보내거나 불러옵니다
+	ch := make(chan int) // 정수형 채널 생성
+
+	go func() {
+		ch <- 10
+	}() // 채널에 10을 보냄
+
+	result := <-ch // 채널로부터 10을 전달받음
+	fmt.Printf("\n%d\n", result)
+
+	// 포인터 연산자
+	var num3 int = 5
+	pnum := &num3
+
+	fmt.Println("\nnum3 :", num3) // num3 값
+	fmt.Println("pnum :", pnum)   // num3의 메모리 주소
+	fmt.Println("pnum :", *pnum)  // num3의 메모리 주소에 할당되어있는 값에 접근
+
+	*pnum++                      // num3의 메모리 주소에 할당돼있는 값에 접근후 1을 더함
+	fmt.Println("num3 :", num3)  // 기존 메모리 주소에 할당되어있는 값에서 1 더한값을 출력
+	fmt.Println("pnum :", *pnum) // 기존 메모리 주소에 할당되어있는 값에서 1 더한값을 출력
 
 }
